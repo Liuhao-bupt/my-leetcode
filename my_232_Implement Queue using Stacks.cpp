9e6ae1ada73d032447c28fe7892b6e364b26f677
Liuhao-bupt <Liuhao_bupt@163.com>
2016-05-22 11:39:40 +0800

    Create my_232_Implement Queue using Stacks.cpp

---
+++ my_232_Implement Queue using Stacks.cpp
@@ -0,0 +1,34 @@
+class Queue {
+public:
+    stack<int> in;
+    stack<int> out;
+    // Push element x to the back of queue.
+    void push(int x) {
+        in.push(x);
+    }
+    void move(){
+        while(!in.empty()){
+            int x = in.top();
+            in.pop();
+            out.push(x);
+        }
+    }
+    // Removes the element from in front of queue.
+    void pop(void) {
+        if(out.empty())
+            move();
+        out.pop();
+    }
+
+    // Get the front element.
+    int peek(void) {
+        if(out.empty())
+            move();
+        return out.top();
+    }
+
+    // Return whether the queue is empty.
+    bool empty(void) {
+        return in.empty() && out.empty();
+    }
+};
