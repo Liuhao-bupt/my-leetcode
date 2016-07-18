ce72c7d942d09872b7290397dfbd9c2605213318
Liuhao-bupt <Liuhao_bupt@163.com>
2016-07-18 14:19:06 +0800

    Create my_048_Rotate Image.cpp

---
+++ my_048_Rotate Image.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(), matrix.end());
+        int n = matrix.size();
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j)
+                swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+};
